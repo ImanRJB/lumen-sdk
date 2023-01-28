b90e59314274b1bc97fc015ac1cbe53199a6e0b0
Iman Rajabi <iman.rjb@gmail.com>
2023-01-29 01:01:27 +0330

    first commit

---
+++ app/LumenSdkServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace ImanRjb\LumenSdk;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\ServiceProvider;
+use ImanRjb\JwtAuth\Commands\PurgeRevokedTokens;
+
+class LumenSdkServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // For load config files
+        if (file_exists(__DIR__ . '/../config/services.php')) {
+            $this->mergeConfigFrom(__DIR__ . '/../config/services.php', 'jwt-auth');
+        }
+
+        if (! $this->app instanceof \Illuminate\Foundation\Application) {
+            $this->app->register(\Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
+            $this->app->register(\Irazasyed\Larasupport\Providers\ArtisanServiceProvider::class);
+            $this->app->register(\Anik\Form\FormRequestServiceProvider::class);
+        }
+
+        $this->publishes([
+            __DIR__.'/../config/services.php' => lumen_config_path('services.php')
+        ], 'lumen-sdk');
+    }
+
+    public function boot()
+    {
+        if(config('services.remote_authorization')) {
+            $this->app['auth']->viaRequest('api', function ($request) {
+                $token = $request->bearerToken();
+                if ($token) {
+                    try {
+                        $request = Http::withHeaders([
+                            'Authorization' => 'Bearer ' . $token,
+                            'Internal-Secret' => config('services.secret')
+                        ])->get(config('services.auth_endpoint') . '/auth/info/internal');
+                        return $request->json('data');
+                    } catch (\Exception $exception) {
+                        return;
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ config/services.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'secret' => env('INTERNAL_SECRET'),
+    'remote_authorization' => env('REMOTE_AUTHORIZATION', true),
+
+    'auth_endpoint' => env('AUTH_ENDPOINT'),
+];
